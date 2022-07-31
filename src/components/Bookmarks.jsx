6bfe691c516252732b391649ee0910f9eaf9c4bf
jim-low <robil12380@gmail.com>
2022-07-31 14:10:19 +0800

    feat: add interface for Bookmark component

--- src/components/Bookmarks.jsx
+++ src/components/Bookmarks.jsx
@@ -1,6 +1,6 @@
 const Bookmarks = () => {
     return (
-        <div>
+        <div className="bookmarks">
 
         </div>
     )
---
+++ src/interfaces/Bookmark.ts
@@ -0,0 +1,4 @@
+interface Bookmark {
+}
+
+export default Bookmark
